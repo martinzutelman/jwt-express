9b12e0a53e4423371c6784960c02c65d557e8c32
martinzutelman <zutelmanmartin@gmail.com>
2023-10-01 23:16:29 -0300

    started project management
    Added project related models to prisma schema, an added new project script which will enable to to create modify share and delete projects.

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const PORT = 3000;
 
 app.use (cookieParser());
 
-const auth = require('./routes/auth');
 const user = require('./routes/user');
+const auth = require('./routes/auth');
+const project = require('./routes/project');
 
-app.use ('/auth', auth);
 app.use('/user', user);
+app.use ('/auth', auth);
+app.use('/project', project);
 
 app.listen(PORT, () => 
     console.log(`Server running on port ${PORT}`)
